da4038aee8fe9fce58414c1a9639b040783d0fb3
Killua Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2024-06-02 16:23:30 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Password-Generator
+# Password Generator
+
+This repository contains a simple web-based password generator tool built using HTML, CSS, and JavaScript. It allows users to generate customized passwords with various options such as length, type, and strength indicators.
+
+## Features
+
+- **Customizable Password Generation**: Users can specify the length of the password and choose from different generation methods, including random passwords, passphrases, pattern-based passwords, entropy-based passwords, cryptographic hash passwords, and quantum random number passwords.
+- **Password Strength Indicator**: Provides visual feedback on the strength of the generated password based on its length.
+- **Clipboard Integration**: Allows users to easily copy the generated password to the clipboard with a single click.
+
+## Usage
+
+1. Clone or download the repository to your local machine.
+2. Open the `index.html` file in your web browser.
+3. Customize the password options as needed (length, type).
+4. Click the "Generate Password" button to generate a password.
+5. Optionally, click the copy icon to copy the generated password to the clipboard.
+
+## File Structure
+
+- `index.html`: Contains the HTML structure of the password generator tool.
+- `styles.css`: Defines the styles for the HTML elements.
+- `script.js`: Implements the logic for generating passwords and handling user interactions.
+- `README.md`: Provides information about the repository.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,219 @@
+document.addEventListener("DOMContentLoaded", function() { 
+});
+const passwordOutput = document.getElementById('passwordOutput');
+const lengthSlider = document.getElementById('lengthSlider');
+const passwordLengthSpan = document.getElementById('passwordLength');
+const passIndicator = document.querySelector(".pass-indicator");
+const copyIcon = document.getElementById('copyIcon');
+const generateButton = document.getElementById('generateButton');
+
+copyIcon.addEventListener('click', () => {
+    navigator.clipboard.writeText(passwordOutput.value);
+    // Replace the copy icon with a green tick icon temporarily
+    copyIcon.innerHTML = 'check';
+    copyIcon.style.color = '#43A047';
+    setTimeout(() => {
+        // Reset the copy icon to its original state after 2 seconds
+        copyIcon.innerHTML = 'copy_all';
+        copyIcon.style.color = ''; // Reset color
+    }, 2000);
+});
+
+
+generateButton.addEventListener('click', async () => {
+    const length = parseInt(lengthSlider.value);
+    let optionid = '';
+    const checkedOption = document.querySelector('input[name="passwordOption"]:checked');
+    let password = '';
+
+    if (checkedOption) {
+        optionid = checkedOption.id;
+    }
+
+    if (!optionid) {
+        // If no option is selected, default to 'randompass' and check it
+        const option = document.getElementById('randompass');
+        option.checked = true;
+        optionid = option.id;
+    }
+
+    switch (optionid) {
+        case 'randompass':
+            password = generateRandomPassword(length);
+            break;
+        case 'passphrases':
+            password = generatePassphrase(length);
+            break;
+        case 'patternpass':
+            password = generatePatternPassword(length);
+            break;
+        case 'entropy':
+            password = generateEntropyPassword(length);
+            break;
+        case 'cryptographic':
+            const supportedAlgorithms = ['SHA-1', 'SHA-512', 'SHA-384', 'SHA-256'];
+            const algorithm = supportedAlgorithms[Math.floor(Math.random() * supportedAlgorithms.length)];
+            password = await generateCryptographicHashPassword(generateEntropyPassword(length), algorithm, length);
+            break;
+    }
+
+    passwordOutput.value = password;
+});
+
+function generateRandomPassword(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return Array.from({ length }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+}
+
+function generatePassphrase(length) {
+    const words = [
+        // Fruits
+        'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 
+        'mango', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'vanilla', 'watermelon', 
+        // City Names
+        'paris', 'london', 'tokyo', 'newyork', 'berlin', 'sydney', 'mumbai', 'istanbul', 'rome', 'moscow', 
+        'seoul', 'rio', 'cairo', 'athens', 'dublin', 'toronto', 'amsterdam', 'beijing', 'dakar', 'delhi', 
+        // People Names
+        'john', 'mary', 'peter', 'susan', 'michael', 'linda', 'william', 'elizabeth', 'james', 'jennifer', 
+        'robert', 'patricia', 'david', 'barbara', 'richard', 'jessica', 'joseph', 'sarah', 'thomas', 'karen', 
+        // Dishes
+        'pizza', 'sushi', 'pasta', 'burger', 'taco', 'curry', 'soup', 'salad', 'steak', 'sandwich', 
+        'kebab', 'sashimi', 'ramen', 'risotto', 'padthai', 'lasagna', 'samosa', 'pancake', 'gnocchi', 'paella', 
+        // Pet Names
+        'buddy', 'molly', 'max', 'lucy', 'charlie', 'daisy', 'bailey', 'rocky', 'bella', 'jack', 
+        'sadie', 'oliver', 'lily', 'toby', 'sophie', 'cooper', 'rosie', 'sam', 'mia', 'oscar', 
+        // Animal Types
+        'lion', 'tiger', 'elephant', 'zebra', 'giraffe', 'panda', 'kangaroo', 'koala', 'gorilla', 'cheetah', 
+        'rhinoceros', 'hippopotamus', 'crocodile', 'leopard', 'panther', 'wolf', 'fox', 'bear', 'lynx', 'hyena'
+    ];
+    const symbols = {
+        'a': ['@', '4', 'A'],
+        'e': ['3', 'E'],
+        'i': ['1', '!', 'I'],
+        'o': ['0', 'O'],
+        's': ['$', '5', 'S'],
+        't': ['7', 'T']
+    };
+
+    function replaceWithSymbol(word) {
+        return word.split('').map(char => {
+            if (symbols[char.toLowerCase()]) {
+                return symbols[char.toLowerCase()][Math.floor(Math.random() * symbols[char.toLowerCase()].length)];
+            } else {
+                return char;
+            }
+        }).join('');
+    }
+
+    function randomlyCapitalize(word) {
+        return word.split('').map(char => {
+            if (Math.random() < 0.5) {
+                return char.toUpperCase();
+            } else {
+                return char;
+            }
+        }).join('');
+    }
+
+    let passphrase = '';
+    while (passphrase.length < length) {
+        const randomWord = words[Math.floor(Math.random() * words.length)];
+        let modifiedWord = replaceWithSymbol(randomWord);
+        modifiedWord = randomlyCapitalize(modifiedWord);
+        passphrase += modifiedWord;
+    }
+    return passphrase.slice(0, length);
+}
+
+function generatePatternPassword(length) {
+    const simplePatterns = ["qwerty", "asdfgh", "zxcvbn"];
+    const complexPatterns = ["!QAZ2wsx", "1qaz2wsx", "qwe123!@#"];
+    const numericPatterns = ["123456", "987654", "2580"];
+    const alphanumericPatterns = ["abc123", "1a2b3c", "z9y8x7"];
+    const customPatterns = ["Mnbvcxz", "Plmko0", "qazwsx"];
+
+    const allPatterns = [
+        ...simplePatterns,
+        ...complexPatterns,
+        ...numericPatterns,
+        ...alphanumericPatterns,
+        ...customPatterns
+    ];
+
+    function getRandomPattern() {
+        const pattern = allPatterns[Math.floor(Math.random() * allPatterns.length)];
+        if (pattern.length >= length) {
+            return pattern.substring(0, length);
+        } else {
+            let extendedPattern = pattern;
+            while (extendedPattern.length < length) {
+                extendedPattern += pattern;
+            }
+            return extendedPattern.substring(0, length);
+        }
+    }
+
+    return getRandomPattern();
+}
+
+// Function to shuffle a string
+function shuffle(str) {
+    const arr = str.split(''); // Convert string to array
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // Generate random index
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+    }
+    return arr.join(''); // Convert array back to string
+}
+
+function generateEntropyPassword(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    return Array.from({ length }, () => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+}
+
+async function generateCryptographicHashPassword(password, algorithm, length) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+
+    let hashedPassword;
+
+    switch (algorithm) {
+        case 'SHA-1':
+            hashedPassword = await window.crypto.subtle.digest('SHA-1', data);
+            break;
+        case 'SHA-256':
+            hashedPassword = await window.crypto.subtle.digest('SHA-256', data);
+            break;
+        case 'SHA-384':
+            hashedPassword = await window.crypto.subtle.digest('SHA-384', data);
+            break;
+        case 'SHA-512':
+            hashedPassword = await window.crypto.subtle.digest('SHA-512', data);
+            break;
+        default:
+            throw new Error('Unsupported hashing algorithm');
+    }
+
+    // Convert the hashed array buffer to a hex string
+    const hashedPasswordHex = Array.from(new Uint8Array(hashedPassword))
+        .map(byte => byte.toString(16).padStart(2, '0'))
+        .join('');
+
+    const truncatedPassword = hashedPasswordHex.slice(0, length);
+
+    return truncatedPassword;
+}
+
+const updatePassIndicator = () => {
+    passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
+}
+
+const updateSlider = () => {
+    document.querySelector(".pass-length span").innerText = lengthSlider.value;
+    updatePassIndicator();
+}
+
+updateSlider();
+
+
+lengthSlider.addEventListener("input", updateSlider);
